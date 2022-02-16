9c51a64e1fe9c74a37a35cab58647dea21272fe0
Gavrán Tamás <b0gavtam@vasvari.hu>
2022-02-16 14:39:32 +0100

    halmaz 1

--- 08-Halmazok/Feladat 1/main.py
+++ 08-Halmazok/Feladat 1/main.py
@@ -6,6 +6,9 @@ import time
 
 halmaz: List[int] = []
 elemekSzama:int = None
+osszeg:int = None
+ketjegyuszamok = None
+paratlanszamokosszege = None
 
 def hiba(uzenet:str)-> None:
     print(uzenet)
@@ -46,16 +49,72 @@ def halmazKiirasa(kiirandoHalmaz:List[int])-> None:
     for item in kiirandoHalmaz:
         print(f"{item}", end="\t")
 
+def ketjegyuszamokszama(keresesHalmaza:List[int])-> int:
+    eredmeny: int = 0
+    for item in keresesHalmaza:
+        if(abs(item) > 9 and abs(item) < 100):
+            eredmeny +=1
+    
+    return eredmeny
+
+def egyjegyuszamokszama(keresesHalmaza:List[int])-> int:
+    for item in keresesHalmaza:
+        if(abs(item) < 10):
+            print(f"{item}", end="\t")
+    
+def paratlanSzamokOsszege(keresesHalmaza:List[int])-> int:
+    eredmeny:int = 0
+    for item in keresesHalmaza:
+        if(item % 2 == 1):
+            eredmeny += item
+
+    return eredmeny
+
+def parosSzamokKiirasa(kiirandoHalmaz:List[int])-> None:
+    for item in kiirandoHalmaz:
+        if(item % 2 == 0):
+            print(f"{item}", end="\t")
+
+def sum(osszeadandoHalmaz:List[int])-> int:
+    eredmeny:int = 0
+    for item in osszeadandoHalmaz:
+        eredmeny += item
+    
+    return eredmeny
+
 #főprogram
 
 elemekSzama= elemszamBekerese()
 halmaz = ListaFeltoltesekRandomSzamokkal(elemekSzama)
 os.system("cls")
 
-print("A halamz elemei: \t")
+print("A halamaz elemei: \t")
 halmazKiirasa(halmaz)
 
 
 #Írassuk ki fordított sorrendben
 print("\nA halmaz elemeinek kiírása fordított sorrendben: \t")
 halmazKiirasaForditottSorrendben(halmaz)
+
+#Számítsuk ki az elemek összegét
+osszeg = sum(halmaz)
+print(f"\nA halmaz elemeinek összege:{osszeg}")
+
+#Átlagoljuk a tömbelemeket
+print(f"\nA halmaz elemeinek átlaga: {osszeg / elemekSzama}")
+
+#Írassuk ki a páros elemeket
+print(f"\nA halmaz páros elemei:")
+parosSzamokKiirasa(halmaz)
+
+#Számoljuk meg hány kétjegyű szám van a tömbben
+ketjegyuszamok = ketjegyuszamokszama(halmaz)
+print(f"\nA halmazban {ketjegyuszamok} kétjegyű szám van.")
+
+#Írassuk ki az egyjegyű számokat
+print("\nA halmaz egyjegyű elemei:")
+egyjegyuszamokszama(halmaz)
+
+#Számítsuk ki a páratlan számok összegét
+print(f"\n A halmaz páratlan elemeinek összege: {paratlanszamokosszege}")
+paratlanszamokosszege = paratlanSzamokOsszege(halmaz)
